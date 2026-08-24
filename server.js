cc5d1c5be5eb413ca6e21ab3fc58b3a1e30cf82c
Kim Taehoon <xogns00089@gmail.com>
2026-08-24 13:00:26 +0900

    fix: serve uploads folder and show attached files in application detail

--- server.js
+++ server.js
@@ -103,6 +103,7 @@ app.use((req, res, next) => {
 
 // 정적 파일
 app.use(express.static("public"));
+app.use("/uploads", express.static("uploads")); // アップロードファイルを公開
 
 // 로그인 API
 app.post("/login", async (req, res) => {
